b4fe3bd9b33be753f0da761649a5e98450211bd3
Amaury Higa <amauryhiga@gmail.com>
2015-04-22 17:33:16 -0300

    Pixel Bro Jump Ajustes
    Pixel Bro Jump Ajustes

---
+++ PixelBroGame/New Unity Project/Assets/Scripts/PlayerColliderListener.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerColliderListener : MonoBehaviour {
+
+	public PlayerStateListener targetStateListener = null;
+
+
+	void OnTriggerEnter2D(Collider2D collidedObject){
+		switch(collidedObject.tag){
+
+			case "Platform":
+				
+				// When the player lands on a platform, toggle the Landing state.
+				targetStateListener.onStateChange(PlayerStateController.playerStates.landing);
+
+				break;
+
+		}
+	}
+
+
+}
--- PixelBroGame/New Unity Project/Assets/Scripts/PlayerStateListener.cs
+++ PixelBroGame/New Unity Project/Assets/Scripts/PlayerStateListener.cs
@@ -6,7 +6,12 @@ public class PlayerStateListener : MonoBehaviour {
 	public float playerWalkSpeed = 3f;
 	private bool playerHasLanded = true;
 
+	//JUMP
+	public float playerJumpForceVertical = 500f;
+	public float playerJumpForceHorizontal = 250f;
+
 	private Animator playerAnimator = null;
+	private Rigidbody2D rigidbody2D;
 
 	private PlayerStateController.playerStates previousState = PlayerStateController.playerStates.idle;
 	private PlayerStateController.playerStates currentState = PlayerStateController.playerStates.idle;
@@ -24,6 +29,7 @@ public class PlayerStateListener : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
 		playerAnimator = GetComponent<Animator>();
+		rigidbody2D = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
@@ -48,21 +54,25 @@ public class PlayerStateListener : MonoBehaviour {
 				break;
 				
 			case PlayerStateController.playerStates.left:
+					
 					transform.Translate(new Vector3((playerWalkSpeed * -1.0f) * Time.deltaTime, 0.0f, 0.0f));
 					if(localScale.x > 0.0f)
 					{
 						localScale.x *= -1.0f;
 						transform.localScale  = localScale;
 					}
+												
 				break;
 				
 			case PlayerStateController.playerStates.right:
+					
 					transform.Translate(new Vector3(playerWalkSpeed * Time.deltaTime, 0.0f, 0.0f));				
 					if(localScale.x < 0.0f)
 					{
 						localScale.x *= -1.0f;
 						transform.localScale = localScale;              
 					}
+					
 				break;
 		}
 	}
@@ -70,6 +80,14 @@ public class PlayerStateListener : MonoBehaviour {
 
 	public void onStateChange(PlayerStateController.playerStates newState)
 	{
+
+		//CONDICAO PARA LOOP
+		if(!playerHasLanded){
+			if(!inJumpAction(newState))
+				return;
+		}
+			
+
 
 		if(newState == currentState)
 			return;
@@ -98,13 +116,37 @@ public class PlayerStateListener : MonoBehaviour {
 					playerAnimator.SetInteger("StatAnim", 1);
 				break;
 
-		case PlayerStateController.playerStates.jump:                   
-			if(playerHasLanded)
-			{
-				// Use the jumpDirection variable to specify if the player should be jumping left, right or vertical
-				Debug.Log("jump");
-			}
+			case PlayerStateController.playerStates.jump:                   
+				if(playerHasLanded)
+				{
+					// Use the jumpDirection variable to specify if the player should be jumping left, right or vertical
+					float jumpDirection = 0.0f;
+					if(currentState == PlayerStateController.playerStates.left){
+						jumpDirection = -1.0f;
+					    //rigidbody2D.velocity = new Vector2 (jumpDirection* playerWalkSpeed, rigidbody2D.velocity.y);
+
+
+					}else if(currentState == PlayerStateController.playerStates.right){
+						jumpDirection = 1.0f;
+					    
+					}else{
+						jumpDirection = 0.0f;
+					}
+					
+					// Apply the actual jump force
+					rigidbody2D.AddForce(new Vector2(jumpDirection * playerJumpForceHorizontal, playerJumpForceVertical));
+					
+					playerHasLanded = false;
+					PlayerStateController.stateDelayTimer[ (int)PlayerStateController.playerStates.jump] = 0f;
+				}
+				break;
+
+			case PlayerStateController.playerStates.landing:
+				
+				playerHasLanded = true;
+				PlayerStateController.stateDelayTimer[(int)PlayerStateController.playerStates.jump]= Time.time + 0.1f;
 			break;
+
 
 
 		}
@@ -140,13 +182,41 @@ public class PlayerStateListener : MonoBehaviour {
 
 
 		case PlayerStateController.playerStates.jump:
-			// The only state that can take over from Jump is landing or kill.		
-			returnVal = true;
+			// The only state that can take over from Jump is landing or kill.
+			if(
+				newState == PlayerStateController.playerStates.landing
+				|| newState == PlayerStateController.playerStates.kill
+				|| newState == PlayerStateController.playerStates.firingWeapon			
+				)
+				returnVal = true;
+			else
+				returnVal = false;
 		
 			break;
 
+		case PlayerStateController.playerStates.landing:
+			// The only state that can take over from landing is idle, left or right movement.
+			if(
+				newState == PlayerStateController.playerStates.left
+				|| newState == PlayerStateController.playerStates.right
+				|| newState == PlayerStateController.playerStates.idle
+				|| newState == PlayerStateController.playerStates.firingWeapon
+				)
+				returnVal = true;
+			else
+				returnVal = false;
+			break; 
+
 		}          
+
+
+
+
 		return returnVal;
+
+
+
+
 	}
 
 	bool checkIfAbortOnStateCondition(PlayerStateController.playerStates newState)
@@ -163,17 +233,45 @@ public class PlayerStateListener : MonoBehaviour {
 				
 			case PlayerStateController.playerStates.right:
 				break;
-		case PlayerStateController.playerStates.jump:
-			float nextAllowedJumpTime = PlayerStateController.stateDelayTimer[ (int)PlayerStateController.playerStates.jump ];
-			
-			if(nextAllowedJumpTime == 0.0f || nextAllowedJumpTime > Time.time)
-				returnVal = true;
-			break;
+
+			case PlayerStateController.playerStates.jump:
+
+				float nextAllowedJumpTime = PlayerStateController.stateDelayTimer[ (int)PlayerStateController.playerStates.jump ];				
+				if(nextAllowedJumpTime == 0.0f || nextAllowedJumpTime > Time.time){
+					returnVal = true;
+				}								
+				break;
+
+			case PlayerStateController.playerStates.landing:				
+				break;
+
+
 			
 
 		}
 		
 		// Value of true means 'Abort'. Value of false means 'Continue'.
+		return returnVal;
+	}
+
+	bool inJumpAction(PlayerStateController.playerStates newState){
+
+		Vector3 localScale = transform.localScale;		
+		transform.localEulerAngles = Vector3.zero;
+
+		switch (newState) {
+			case PlayerStateController.playerStates.right:
+						rigidbody2D.velocity = new Vector2 (1 * 3.5f, rigidbody2D.velocity.y);						
+				break;
+				
+			case PlayerStateController.playerStates.left:		
+						rigidbody2D.velocity = new Vector2 (-1 * 3.5f, rigidbody2D.velocity.y);
+						
+				break;
+		}
+
+
+		bool returnVal = true;
 		return returnVal;
 	}
 
